5cfa0c2573e3620b31aab9f84bf075da7f1613e1
falese <falese@gmail.com>
2015-07-30 12:29:23 -0700

    updated calc.rb

--- SeanFalese-falese/hw_1/calc.rb
+++ SeanFalese-falese/hw_1/calc.rb
@@ -1,1 +1,8 @@
-puts 1+2
+puts 'Do you want to play a game?'
+name = gets.chomp
+if
+  name =='Yes'
+  puts 'how about a nice game of chess?'
+elsif name == 'No'
+  puts 'thermonuclear war it is then.'
+end
