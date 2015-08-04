6e8b5bd9a37dbc4ecaa7e1e66cdb8c67a614484e
rayliaw <ray.liaw@gmail.com>
2015-08-04 15:39:53 -0700

    replace while loop with each_index

--- RaymondLiaw-rayliaw/reverse_it.rb
+++ RaymondLiaw-rayliaw/reverse_it.rb
@@ -13,21 +13,15 @@ def is_palindrome(str)
   end
 end
 
-# Returns a new array in the reversed order without using the reverse method
+# Returns a new array in reverse order without using the reverse method
 def reverse_array(arr)
-  i = 0
+  # Instantiate array with same length as passed array
   flipped_arr = Array.new(arr.length)
-
-  # Loop through the passed array and save to a new array in reverse order
-  while i < arr.length
-    flipped_arr[arr.length-1-i] = arr[i]
-    i += 1
-  end
-
-  # Return the new array
+  # Enumerate an array and save value to the reversed position in flipped_array
+  arr.each_index { |e| flipped_arr[arr.length-1-e] = arr[e] }
+  # Return the new reversed array
   return flipped_arr
 end
-
 
 # INIT
 puts "Please provide a word or phrase:"
