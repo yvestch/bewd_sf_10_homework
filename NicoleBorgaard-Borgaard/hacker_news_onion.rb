f6f90465a40d914195d254d12e36511fe6bfc478
Borgaard <nicoleborgaard@gmail.com>
2015-08-03 19:22:23 -0700

    add hackernewsonion

---
+++ NicoleBorgaard-Borgaard/hacker_news_onion.rb
@@ -0,0 +1,17 @@
+
+#https://twitter.com/HackerNewsOnion
+Programming is 1% inspiration, 99% trying to get your environment working.
+-----------
+`git push --force`
+I got 99 problems but a merge conflict aint one.
+-----------
+Developer Accused Of Unreadable Code Refuses To Comment
+-----------
+Developer at 24hr Hackathon Gets Development Environment Working With Two Whole Hours To Go
+-----------
+Daft Punk admits #1 hit song "Up All Night To Get Lucky" is actually about programming
+-----------
+Enterprise CTO not impressed by pair programming.
+"We often have hundreds of developers working on the same thing."
+-----------
+Obama – "The national technical debt is increasing at an alarming rate. We have to stop writing PHP."
