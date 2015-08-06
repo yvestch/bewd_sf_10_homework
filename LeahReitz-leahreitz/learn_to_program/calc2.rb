0b0327381c26415d5f99b1be59e3eb3625b9b3c8
leahreitz <leah.reitz@gmail.com>
2015-08-05 18:27:32 -0700

    test

---
+++ LeahReitz-leahreitz/learn_to_program/calc2.rb
@@ -0,0 +1,13 @@
+puts 'HELLO, IT\'S YOUR DEAF GRANDMA!'
+answer = gets.chomp
+if
+  answer = answer.downcase
+  puts 'HUH? SPEAK UP, SUNNY!'
+else
+  puts 'NO, NOT SINCE 1983'
+if
+  answer = answer.upcase
+  puts 'NO, NOT SINCE 1938!'
+else
+end
+end
