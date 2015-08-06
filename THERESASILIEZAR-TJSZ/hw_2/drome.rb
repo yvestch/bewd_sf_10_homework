30ae3658ebfaa2a25a72c40d1167357dcaa231e9
tjsz <tjsz@icloud.com>
2015-08-05 18:32:40 -0700

    homework 2

---
+++ THERESASILIEZAR-TJSZ/hw_2/drome.rb
@@ -0,0 +1,12 @@
+def my_reverse (string)
+  Puts "What is a Palindrome?"
+  answer = gets.downcase
+  if answer.downcase == answer.downcase.<<
+    "Yes this is a Palindrome!"
+  else
+    "try again"
+  end 
+end
+
+
+
