367b07546875977eb1e3565f98d060234d751312
falese <falese@gmail.com>
2015-08-07 13:09:09 -0700

    update of hash_party.rb

--- SeanFalese-falese/hw_3/hash_party.rb
+++ SeanFalese-falese/hw_3/hash_party.rb
@@ -1,33 +1,37 @@
+#includes here
 require 'pry'
 
-
+# methods up here
 def hash_supply()
-  list = []
-
+    types =[]
+    list = []
   puts "How many things did you bring today??"
-  count = gets.chomp
+    count = gets.chomp
 
+  #loop entry until all items are entered into list array
   begin
+    puts "Place an item in the bag"
     list.push(gets.chomp)
+    puts "and what type of item is it?"
+    types.push(gets.chomp)
   end until count.to_i == list.length
-
-
-
-
-  stash = Hash.new("all out dude")
-  puts "what type of stuff are you putting in the stash"
-  var1 = gets.chomp
-  puts "wow cool, what exactly is it?"
-  var2 = gets.chomp
-  second_hash = Hash.new
-    second_hash["type"] = var1
-    second_hash["stuff"] = list
-
-    # stash.merge!(:uppers, "#{var1}") i was trying to insert a KVP here
+  #create hashes starting here
+    stash = Hash.new("all out dude")
+  # puts "what type of stuff are you putting in the stash"
+  #   var1 = gets.chomp
+    second_hash = Hash.new
+      second_hash["type"] = types
+      second_hash["item"] = list
   stash.merge!(second_hash)
-
-    # stash.merge!(:downers, var2) i was trying to insert a new KVP here
+  puts "The length of this hash is  #{stash.length}"
+  stash.each{|key, value| puts "#{value}, are #{key}s"}
+puts "look for an item in the bag"
+  var1 = gets.chomp
+  if stash.has_value?(var1)
+    puts "Its in the bag"
+  else
+    puts "its not here"
+  end
 end
-
-
-puts hash_supply
+  #begin program here
+hash_supply
