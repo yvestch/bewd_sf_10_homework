a93bf1e78cc4d3277e7f8268f517fd6c8abd3253
falese <falese@gmail.com>
2015-08-07 15:59:24 -0700

    final commit of hw_3

--- SeanFalese-falese/hw_3/hash_party.rb
+++ SeanFalese-falese/hw_3/hash_party.rb
@@ -17,23 +17,23 @@ def hash_supply()
   end until count.to_i == list.length
   #create hashes starting here
     stash = Hash.new("all out dude")
-  # puts "what type of stuff are you putting in the stash"
-  #   var1 = gets.chomp
     second_hash = Hash.new
       second_hash["type"] = types
       second_hash["item"] = list
   stash.merge!(second_hash)
   puts "The length of this hash is  #{stash.length}"
+  puts "here are the keys: #{stash.keys}"
+  puts "here are the values: #{stash.values}"
   stash.each{|key, value| puts "#{value}, are #{key}s"}
-puts "look for an item in the bag"
-  var1 = gets.chomp
-  if stash.has_value?(var1)
-    puts "Its in the bag"
-  else
-    puts "its not here"
-    puts "here are the keys: #{stash.keys}"
-    puts "here are the values: #{stash.values}"
-  end
+
+
+  #this doesn't work because i couldn't figure out how to look in an array in a hash
+  # if stash.to_a.include?(var1)
+  #   puts "Its in the bag"
+  # else
+  #   puts "its not here"
+  #
+  # end
 end
   #begin program here
 hash_supply
