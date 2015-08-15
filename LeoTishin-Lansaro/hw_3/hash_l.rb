3ebdbd2b6246e1df325905522730213f315249c0
Leo Starlord <leonidtsh@gmail.com>
2015-08-14 18:44:14 -0700

    array/hash trial

--- LeoTishin-Lansaro/hw_3/hash_l.rb
+++ LeoTishin-Lansaro/hw_3/hash_l.rb
@@ -13,16 +13,25 @@ end
 def choice(stars)
   puts "Select planet"
   planet = gets.chomp.downcase
-    if stars.include?(planet)
-      puts "Here"
-    else
-      puts "No data. Try again"
+  stars.each do |star|
+    if star[:name] == planet
+      puts star
+      aliens?(star)
+  end
+end
+end
+
+def aliens?(star)
+  if star.has_key?(:population)
+    puts "#{star} - There is someone!"
+  else
+    puts "Dont worry there are no aliens :[ grumpy!"
   end
 end
 
 earth = {name: "earth", size: "medium", location: "3rd planet", population: "~7 bln"}
-mars = {name: "mars", size: "medium", location: "4th planet", population: "0"}
-pluto = {name: "pluto", size: "medium", location: "9th planet", population: "0"}
+mars = {name: "mars", size: "medium", location: "4th planet"}
+pluto = {name: "pluto", size: "medium", location: "9th planet"}
 glieze = {name: "glieze", size: "huge", location: "20.22 bln light years", population: "?"}
 kepler = {name: "kepler", size: "medium", location: "400 mln light years", population: "?"}
 
