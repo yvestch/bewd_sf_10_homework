be138528eb06c7b7f8052b9dfda42a8a153816b2
falese <falese@gmail.com>
2015-08-16 11:37:14 -0700

    commit of get_it.rb homework

--- SeanFalese-falese/hw_4/get_cars_i_love_post.rb
+++ SeanFalese-falese/hw_4/get_cars_i_love_post.rb
@@ -8,7 +8,6 @@ def start
   year = gets.chomp.to_i
   api_url = "https://api.edmunds.com/api/vehicle/v2/#{make}?year=#{year}&view=basic&fmt=json&api_key=n2frga7tjbg5tqq3wyfpf5pw"
   # api_url2 = "https://api.edmunds.com/v1/api/maintenance/servicebulletinrepository/findbymodelyearid?modelyearid=#{yearid}&fmt=json&api_key=n2frga7tjbg5tqq3wyfpf5pw"
-
   connect(api_url)
 end
 
@@ -30,30 +29,17 @@ def parse(get_data)
 end
 
 def array(h1)
-  niceNames=[]
-  ids = []
   h1["models"].each do |car|
-    # niceNames.push(car["niceName"])
     hash(car["niceName"])
-
-
     car["years"].each do |year|
       hash(year["id"])
-
-      # ids.push(year["id"])
     end
   end
-  # h2 = {}
-  #   h2["Model"] = niceNames
-  #   h2["YearID"] = ids
-  # model_pick(niceNames)
-hash(car,year)
 end
 
 def hash(car,year)
   h2 = {model: car["niceName"], yearID: year["id"]}
   puts h2
-
 end
 
 
@@ -65,7 +51,6 @@ def model_pick(niceNames)
     puts "Please select a Model from the list"
     model = gets.chomp
   end
-  service_bulletins(model)
 end
 
 
