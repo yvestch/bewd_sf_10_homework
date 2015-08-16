266d4920779aac6fd33d7a9c0466a6d092502e5d
xrwang <xiaowei.wang@post.harvard.edu>
2015-08-16 07:57:29 -0700

    new stuff

--- XiaoweiWang-xrwang/hw2/xiaoweihomework2.rb
+++ XiaoweiWang-xrwang/hw2/xiaoweihomework2.rb
@@ -1,23 +1,21 @@
 # 1. get a string
 
-word = "xiaowei"
+puts "please enter a word below"
+puts word = gets.strip
 
 
 def reverse_string(word)
   # puts "word: #{word}"
-
   # 2. turn string into an array [a, b, c, d, e]
 
   word_array = word.chars
 
   # puts "word as array: #{word_array}"
-
   # 3. how big (length)
 
   word_length = word_array.size
 
   # puts "word length: #{word_length}"
-
   # 4.          0, 1, 2, 3, 4
 
   reversed_word = []
@@ -35,9 +33,13 @@ def is_palindrome?(word)
   reverse_string(word) == word
 end
 
-puts "'dobis' is a palindrome: #{is_palindrome?("dobis")}"
-puts "'amanaplanacanalpanama' is a palindrome: #{is_palindrome?("amanaplanacanalpanama")}"
-puts "'abba' is a palindrome: #{is_palindrome?("abba")}"
+
+puts "#{word} is a palindrome: #{is_palindrome?(word)}"
+
+
+
+# puts "'amanaplanacanalpanama' is a palindrome: #{is_palindrome?("amanaplanacanalpanama")}"
+# puts "'abba' is a palindrome: #{is_palindrome?("abba")}"
 
 
 #
