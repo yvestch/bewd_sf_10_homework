d08f22243b92ec87c1173cc95f2ffb101686223c
Morgan Holland <morgan.holland@gmail.com>
2015-08-17 19:07:44 -0400

    added Mashable methods to get_it.rb

--- MorganHolland-spoofhopper/hw_05 (get_it)/get_it.rb
+++ MorganHolland-spoofhopper/hw_05 (get_it)/get_it.rb
@@ -22,18 +22,29 @@ def connect_to_api(url)
   JSON.parse(get_data)
 end
 
-def find_stories(get_data) # can call the argument anything, this is just good etiquette to pass the variable name from one method to another
+##modify or duplicate to take Mashable
+def find_reddit_stories(get_data) # can call the argument anything, this is just good etiquette to pass the variable name from one method to another
   stories = get_data['data']['children'] #returns the top 25 stories in an Array
-  puts "Reddit has blessed us with #{stories.length} stories"
+  puts "We've been blessed with #{stories.length} stories!"
   return stories
 end
 
-def print_stories(stories)
+#QUESTION: I thought about putting an 'if' switch in this method to let an additional argument determine whether the source was reddit or mashable but decided two methods was simpler. Seems inefficient given there's a lot of repeat code but not sure how to deal with the fact that stories are stored at different hash/array 'addresses' in reddit vs. mashable, thoughts?
+def find_mashable_stories(get_data) # can call the argument anything, this is just good etiquette to pass the variable name from one method to another
+  stories = get_data['new'] #returns the top 10 new stories in an Array
+  puts "We've been blessed with #{stories.length} stories!"
+  #binding.pry
+  return stories
+end
+
+
+##modify or duplicate to take Mashable
+def print_reddit_stories(stories)
   stories.each do |story|
-    text = story['data']['selftext']
-    title = story['data']['title']
-    category = story['data']['subreddit']
-    upvotes = story['data']['ups']
+    #text = story['data']['selftext']
+    #title = story['data']['title']
+    #category = story['data']['subreddit']
+    #upvotes = story['data']['ups']
     #puts "Title: #{title}"
     #puts "Category: #{category}"
     #puts "Upvotes: #{upvotes}"
@@ -41,10 +52,23 @@ def print_stories(stories)
   end
 end
 
+def print_mashable_stories(stories)
+  stories.each do |story|
+    create_mashable_story_hash(story)
+  end
+end
+
+
 def create_story_hash(story)
   story_hash = {Title: story['data']['title'], Category: story['data']['subreddit'], Upvotes: story['data']['ups']}
   push_story_array(story_hash)
 end
+
+def create_mashable_story_hash(story)
+  story_hash = {Title: story['title'], Category: story['channel'], Upvotes: story['shares']}
+  push_story_array(story_hash)
+end
+
 
 
 def push_story_array(story_hash)
@@ -58,13 +82,18 @@ end
 
 def write_to_CSV(story_array)
   CSV.open("./story_csv.csv", "a+") do |csv|
-    csv << [story_array[0][:Title],story_array[0][:Category],story_array[0][:Upvotes]]
+    csv << [story_array[0][:Title],story_array[0][:Category],story_array[0][:Upvotes]] #Breaks down shares by social channel for Mashable FYI
   end
 end
 
 
 reddit_url = "http://www.reddit.com/.json"
 mashable_url = "http://mashable.com/stories.json"
-reddit_json_responses = connect_to_api(reddit_url)
-stories  = find_stories(reddit_json_responses)
-print_stories(stories)
+
+#reddit_json_responses = connect_to_api(reddit_url)
+#reddit_stories  = find_reddit_stories(reddit_json_responses)
+#print_reddit_stories(reddit_stories)
+
+mashable_json_responses = connect_to_api(mashable_url)
+mashable_stories  = find_mashable_stories(mashable_json_responses)
+print_mashable_stories(mashable_stories)
