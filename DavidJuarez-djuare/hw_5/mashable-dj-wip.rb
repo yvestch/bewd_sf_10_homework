7fcd640dc6acd8d82d73983647f4550a35e2bd87
djuare <djuare@gmail.com>
2015-08-17 17:22:38 -0700

    Homework 5 - David Juarez

--- DavidJuarez-djuare/hw_5/mashable-dj-wip.rb
+++ DavidJuarez-djuare/hw_5/mashable-dj-wip.rb
@@ -15,23 +15,21 @@ require 'rest-client'
 require 'pry'
 require 'json'
 
-
 def connect_to_api(url)
   response = RestClient.get(url)
   JSON.parse(response)
 end
 
-
 def find_stories(response)
-  stories = response["new"]
-  puts "*** Mashable has blessed us with #{stories.count} ***"
-  return stories
+  new_stories = response["new"]
+  new_rising = response["rising"]
+  new_hot = response["hot"]
+  puts "*** Mashable (new) has blessed us with #{new_stories.count} ***"
+  puts "*** Mashable (rising) has blessed us with #{new_rising.count} ***"
+  puts "*** Mashable (hot) has blessed us with #{new_hot.count} ***"
+  return [new_stories, new_rising, new_hot]
+  #return new_stories
 end
-
-  #stories2 = response["rising"]
-  #stories3 = response["hot"]
-  #return_values {stories: return stories
-
 
 def print_stories(stories)
   stories.each do |story|
@@ -40,16 +38,53 @@ def print_stories(stories)
 end
 
 
+# Rising
+
+def print_rising(rising_stories)
+  rising_stories.each do |story|
+    create_rising_hash(story)
+  end
+end
+
+# Rising
+
+# hot
+def print_hot(hot_stories)
+  hot_stories.each do |story|
+    create_hot_hash(story)
+  end
+end
+# hot
 
 def create_story_hash(story)
   new_story = {category: story["channel_label"], title: story["title"], upvotes: story["shares"]["total"]}
   puts new_story
-
 end
 
+# Rising
+def create_rising_hash(story)
+  new_rising = {category: story["channel_label"], title: story["title"], upvotes: story["shares"]["total"]}
+  puts new_rising
+end
+# Rising
+
+# hot
+def create_rising_hash(story)
+  new_hot = {category: story["channel_label"], title: story["title"], upvotes: story["shares"]["total"]}
+  puts new_hot
+end
+# hot
 
 mashable_url = "http://mashable.com/stories.json" 
 mashable_json_response = connect_to_api(mashable_url)
-stories = find_stories(mashable_json_response)
-print_stories(stories)
+
+new_stories = find_stories(mashable_json_response)
+rising_stories = find_stories(mashable_json_response)
+hot_stories = find_stories(mashable_json_response)
+
+print_stories(new_stories)
+print_rising(rising_stories)
+print_hot(hot_stories)
+
+
 
--- DavidJuarez-djuare/hw_5/mashable.rb
+++ DavidJuarez-djuare/hw_5/mashable.rb
@@ -15,21 +15,23 @@ require 'rest-client'
 require 'pry'
 require 'json'
 
+
 def connect_to_api(url)
   response = RestClient.get(url)
   JSON.parse(response)
 end
 
+
 def find_stories(response)
-  new_stories = response["new"]
-  new_rising = response["rising"]
-  new_hot = response["hot"]
-  puts "*** Mashable (new) has blessed us with #{new_stories.count} ***"
-  puts "*** Mashable (rising) has blessed us with #{new_rising.count} ***"
-  puts "*** Mashable (hot) has blessed us with #{new_hot.count} ***"
-  return [ new_stories, new_rising, new_hot ]
-  #return new_stories
+  stories = response["new"]
+  puts "*** Mashable has blessed us with #{stories.count} ***"
+  return stories
 end
+
+  #stories2 = response["rising"]
+  #stories3 = response["hot"]
+  #return_values {stories: return stories
+
 
 def print_stories(stories)
   stories.each do |story|
@@ -37,14 +39,11 @@ def print_stories(stories)
   end
 end
 
-#def create_story_hash(story)
-def create_story_hash(new_story)
+
+
+def create_story_hash(story)
   new_story = {category: story["channel_label"], title: story["title"], upvotes: story["shares"]["total"]}
-  new_rising = {category: story["channel_label"], title: story["title"], upvotes: story["shares"]["total"]}
-  new_hot = {category: story["channel_label"], title: story["title"], upvotes: story["shares"]["total"]}
   puts new_story
-  puts new_rising
-  puts new_hot
 
 end
 
